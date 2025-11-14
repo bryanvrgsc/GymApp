82c60e4311cb387b30752d984456a96c8c67b047
Bryan Vargas <bryanvrgsc@gmail.com>
2025-11-14 16:13:00 -0600

    Initial commit

---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Bryan Vargas on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
